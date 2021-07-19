695b7a0c1fbf0c05a79898ab109229d86a7ca0c8
apoorvpandey0 <apoorvpandey0@gmail.com>
2021-07-19 21:17:52 +0530

    added this question

---
+++ src/Q6.java
@@ -0,0 +1,11 @@
+// 6. Program to demonstrate package concept.
+
+import com.hello.Q6Package;
+
+public class Q6 {
+    public static void main(String[] args) {
+        // call printPoem from Q6Package
+        Q6Package.printPoem();
+
+    }
+}
---
+++ src/com/hello/Q6Package.java
@@ -0,0 +1,20 @@
+
+// Program to implement package concept
+package com.hello;
+
+public class Q6Package {
+        public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+    }
+    // function to print a poem
+    public static void printPoem() {
+        System.out.println("\n\n\n");
+        System.out.println("My life is like a onion");
+        System.out.println("Which I have toiled through");
+        System.out.println("But now I see");
+        System.out.println("I have but one");
+        System.out.println("And that is all");
+        System.out.println("\n\n\n");
+    }
+}
